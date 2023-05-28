57c9a9e39645aae95607add8a1a4608595916c1c
Francheska Reynoso <franceskareynoso@gmail.com>
2023-05-28 19:42:27 +0800

    negamax with alphabeta pruning algo

--- negamax.js
+++ negamax.js
@@ -8,7 +8,7 @@ var myWin = []; // Array to store the player's winning patterns count
 var computerWin = []; // Array to store the AI's winning patterns count
 
 // Initialize the data
-var initData = function() {
+var initData = function () {
   for (var i = 0; i < 15; ++i) {
     chessBoard[i] = [];
     for (var j = 0; j < 15; ++j) {
@@ -23,21 +23,21 @@ var initData = function() {
     }
   }
 
-  // Horizontal winning patterns
-  for (var i = 0; i < 15; ++i) {
-    for (var j = 0; j < 11; ++j) {
-      for (var k = 0; k < 5; ++k) {
-        wins[i][j + k][count] = true;
-      }
-      count++;
-    }
-  }
-
   // Vertical winning patterns
   for (var i = 0; i < 15; ++i) {
     for (var j = 0; j < 11; ++j) {
       for (var k = 0; k < 5; ++k) {
         wins[j + k][i][count] = true;
+      }
+      count++;
+    }
+  }
+
+  // Horizontal winning patterns
+  for (var i = 0; i < 15; ++i) {
+    for (var j = 0; j < 11; ++j) {
+      for (var k = 0; k < 5; ++k) {
+        wins[i][j + k][count] = true;
       }
       count++;
     }
@@ -70,23 +70,52 @@ var initData = function() {
 };
 
 // Draw the chess board
-var drawChessBoard = function() {
+var drawChessBoard = function () {
   var chess = document.getElementById('chess');
   var context = chess.getContext('2d');
   context.clearRect(0, 0, 450, 450);
+  context.strokeStyle = '#bfbfbf';
 
   for (var i = 0; i < 15; ++i) {
+    //draw horizontal lines
     context.moveTo(15 + i * 30, 15);
     context.lineTo(15 + i * 30, 435);
     context.stroke();
+    //draw vertical lines
     context.moveTo(15, 15 + i * 30);
     context.lineTo(435, 15 + i * 30);
     context.stroke();
   }
 };
 
-// Play a chess move
-var playChess = function(e) {
+// Draw a chess piece
+var oneStep = function (i, j, me) {
+  var chess = document.getElementById('chess');
+  var context = chess.getContext('2d');
+  context.beginPath();
+  context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI);
+  context.closePath();
+  var gradient = context.createRadialGradient(
+    15 + i * 30 + 2,
+    15 + j * 30 - 2,
+    13,
+    15 + i * 30 + 2,
+    15 + j * 30 - 2,
+    0
+  );
+  if (me) {
+    gradient.addColorStop(0, '#0A0A0A');
+    gradient.addColorStop(1, '#636766');
+  } else {
+    gradient.addColorStop(0, '#D1D1D1');
+    gradient.addColorStop(1, '#F9F9F9');
+  }
+  context.fillStyle = gradient;
+  context.fill();
+};
+
+// Click event handler for playing chess
+var playChess = function (e) {
   if (over || !me) {
     return;
   }
@@ -114,70 +143,44 @@ var playChess = function(e) {
     }
 
     if (!over) {
-      me = !me;
-      computerAI();
+      //me = !me;
+      //computerAI();
       //setTimeout(computerAI, 500);
+      setTimeout(computerAI, 0);
     }
   }
-};
-
-// Draw a chess piece at the specified position
-var oneStep = function(i, j, me) {
-  var chess = document.getElementById('chess');
-  var context = chess.getContext('2d');
-  context.beginPath();
-  context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI);
-  context.closePath();
-  var gradient = context.createRadialGradient(
-    15 + i * 30 + 2,
-    15 + j * 30 - 2,
-    13,
-    15 + i * 30 + 2,
-    15 + j * 30 - 2,
-    0
-  );
-  if (me) {
-    gradient.addColorStop(0, '#0A0A0A');
-    gradient.addColorStop(1, '#636766');
-  } else {
-    gradient.addColorStop(0, '#D1D1D1');
-    gradient.addColorStop(1, '#F9F9F9');
-  }
-  context.fillStyle = gradient;
-  context.fill();
 };
 
 // Negamax algorithm with alpha-beta pruning
-var negamax = function(alpha, beta, depth, color) {
-if (depth === 0 || over) {
-  return color * evaluate();
-}
+var negamax = function (depth, alpha = -Infinity, beta = Infinity, color) {
+  if (depth === 0 || over) {
+    return color * evaluate();
+  }
 
-var bestValue = -Infinity;
+  var bestValue = -Infinity;
 
-for (var i = 0; i < 15; ++i) {
-  for (var j = 0; j < 15; ++j) {
-    if (chessBoard[i][j] === 0) {
-      chessBoard[i][j] = color;
-      var value = -negamax(-beta, -alpha, depth - 1, -color); // Fix: Negate the color parameter
-      chessBoard[i][j] = 0;
+  for (var i = 0; i < 15; ++i) {
+    for (var j = 0; j < 15; ++j) {
+      if (chessBoard[i][j] === 0) {
+        chessBoard[i][j] = color;
+        var value = -negamax(-beta, -alpha, depth - 1, -color); // Fix: Negate the color parameter
+        chessBoard[i][j] = 0;
 
-      bestValue = Math.max(bestValue, value);
-      alpha = Math.max(alpha, value);
+        bestValue = Math.max(bestValue, value);
+        alpha = Math.max(alpha, value);
 
-      if (alpha >= beta) {
-        return alpha; // Beta cutoff
+        if (alpha >= beta) {
+          return alpha; // Beta cutoff
+        }
       }
     }
   }
-}
 
-return bestValue;
+  return bestValue;
 };
 
-
 // Evaluate the score of the current game state
-var evaluate = function() {
+var evaluate = function () {
   var score = 0;
 
   // Evaluate horizontal patterns
@@ -248,7 +251,7 @@ var evaluate = function() {
 };
 
 // Evaluate the score of a pattern
-var evaluatePattern = function(count, empty) {
+var evaluatePattern = function (count, empty) {
   if (count === 5) {
     return 100000; // Five in a row
   } else if (count === 4 && empty === 1) {
@@ -265,50 +268,57 @@ var evaluatePattern = function(count, empty) {
 };
 
 // AI's turn to play
-var computerAI = function() {
-if (!me) {
-  return;
-}
+var computerAI = function () {
+  if (!me) {
+    return;
+  }
 
-var maxScore = -Infinity;
-var maxX = 0;
-var maxY = 0;
+  var maxScore = -Infinity;
+  var maxX = 0;
+  var maxY = 0;
 
-for (var i = 0; i < 15; ++i) {
-  for (var j = 0; j < 15; ++j) {
-    if (chessBoard[i][j] === 0) {
-      chessBoard[i][j] = 2;
-      var score = -negamax(-Infinity, Infinity, 3, -1); // Adjust the depth parameter to the desired value
-      chessBoard[i][j] = 0;
+  for (var i = 0; i < 15; ++i) {
+    for (var j = 0; j < 15; ++j) {
+      if (chessBoard[i][j] === 0) {
+        chessBoard[i][j] = 2;
+        var score = -negamax(-Infinity, Infinity, 3, -1); // Adjust the depth parameter to the desired value
+        chessBoard[i][j] = 0;
 
-      if (score > maxScore) {
-        maxScore = score;
-        maxX = i;
-        maxY = j;
+        if (score > maxScore) {
+          maxScore = score;
+          maxX = i;
+          maxY = j;
+        }
       }
     }
   }
-}
 
-oneStep(maxX, maxY, false);
-chessBoard[maxX][maxY] = 2;
+  setTimeout(function () {
+    oneStep(maxX, maxY, false);
+    chessBoard[maxX][maxY] = 2;
 
-for (var k = 0; k < count; ++k) {
-  if (wins[maxX][maxY][k]) {
-    computerWin[k]++;
-    myWin[k] = 6; // Invalidate the player's winning on this pattern
-    if (computerWin[k] === 5) {
-      window.alert('AI Wins!');
-      over = true;
+    for (var k = 0; k < count; ++k) {
+      if (wins[maxX][maxY][k]) {
+        computerWin[k]++;
+        myWin[k] = 6; // Invalidate the player's winning on this pattern
+        if (computerWin[k] === 5) {
+          window.alert('AI Wins!');
+          over = true;
+        }
+      }
     }
-  }
-}
 
-if (!over) {
-  //me = !me;
-  requestAnimationFrame(playChess);
-}
+    if (!over) {
+      me = !me;
+    }
+  }, 300);
 };
+
+
+
+
+
+
 
 
 // Event listener for mouse click on the canvas
